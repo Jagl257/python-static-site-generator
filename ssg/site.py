486da310986f76b0b759eee587236a486b5c1ab3
Jagl257 <jagl257@gmail.com>
2021-08-29 22:58:26 -0500

    Fix variables

--- ssg/site.py
+++ ssg/site.py
@@ -1,5 +1,5 @@
 from os import mkdir
-from pathlin import Path 
+from pathlib import Path 
 
 class Site():
 
@@ -9,13 +9,13 @@ class Site():
 
     def create_dir(self, path):
         #directory = f"{self._dest}/{self._source.relative_to(path)}"
-        directory = self.dest / self.source.relative_to(path)
+        directory = self.dest / path.relative_to(self.source)
 
-        mkdir(directory, parents=True, exist_ok=True)
+        directory.mkdir(parents=True, exist_ok=True)
 
     def build(self):
 
-        mkdir(self.dest, parents=True, exist_ok=True)
+        self.dest.mkdir(self.dest, parents=True, exist_ok=True)
 
         for path in self.source.rglob("*"):
             if path.is_dir():
