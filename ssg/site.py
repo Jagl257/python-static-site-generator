ca185e43fa987cb0f51e4340397e9a09b2d85338
Jagl257 <jagl257@gmail.com>
2021-08-29 22:53:53 -0500

    Remove private attributes

--- ssg/site.py
+++ ssg/site.py
@@ -1,23 +1,23 @@
 from os import mkdir
-import pathlib 
+from pathlin import Path 
 
 class Site():
 
     def __init__(self, source, dest) -> None:
-        self._source = pathlib.Path(source)
-        self._dest = pathlib.Path(dest)
+        self.source = Path(source)
+        self.dest = Path(dest)
 
     def create_dir(self, path):
         #directory = f"{self._dest}/{self._source.relative_to(path)}"
-        directory = self._dest / self._source.relative_to(path)
+        directory = self.dest / self.source.relative_to(path)
 
         mkdir(directory, parents=True, exist_ok=True)
 
     def build(self):
 
-        mkdir(self._dest, parents=True, exist_ok=True)
+        mkdir(self.dest, parents=True, exist_ok=True)
 
-        for path in self._source.rglob("*"):
+        for path in self.source.rglob("*"):
             if path.is_dir():
                 self.create_dir(path)
                 
